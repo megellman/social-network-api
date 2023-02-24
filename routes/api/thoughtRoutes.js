0f682d6168cf3a5dd90f22d1b361134c9e5f9709
Megan Ellman <meganlellman@gmail.com>
2023-02-24 13:56:09 -0800

    added api routes and outlines

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,15 @@
+// /
+// GET all thoughts
+
+// GET a single thought by its id
+
+// POST a new thought (push thought's id to user's thought array)
+
+// PUT update a thought by its id
+
+// DELETE a thought by it's id
+
+// :thoughtId/reactions
+// POST a reaction in a single thought's reactions array field
+
+// DELETE pull and remove a reaction by the reaction's reactionId value
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,17 @@
+// /
+// GET all users
+
+// GET a single user by id and populate thought and friend data
+
+// POST a new user
+
+// PUT update a user by its id
+
+// DELETE a user by its id
+
+// BONUS Remove a user's associated thoughts when deleted
+
+// /:userId/friends/:friendId
+// POST add a new friend to user's friend list
+
+// DELETE a friend from user's friend list
